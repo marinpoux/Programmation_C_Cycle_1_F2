fd9d4d2e48bf0c99d1824f030dc42472c80ee371
marine schwartz <m.schwartz@ludus-academie.com>
2020-11-18 12:54:26 +0100

    pointeurs, passage par adresse

---
+++ programmation_c/define.h
@@ -0,0 +1,9 @@
+#ifndef DEFINE_H_INCLUDED
+#define DEFINE_H_INCLUDED
+#define TAILLE 10
+
+//IMPORTANT: précision variables EXTERN !!
+
+extern int fctInit_tabEntier(int n_tabEntier[TAILLE]);  //rend la fonction accessible partout ailleurs dans le programme, tant qu'on retse dans le même projet
+
+#endif // DEFINE_H_INCLUDED
--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//prototypes
+    void jet_de_des(int *place, int const jet1, int const jet2);
+
 int main()
 {
-    printf("Hello world!\n");
+    //procedure
+    int place=0;
+    int jet1=10;
+    int jet2=10;
+    //appel et passage par adresse
+    jet_de_des(&place,jet1,jet2);
+
     return 0;
+}
+
+
+//procedure et passage par adresse -->modification valeur en argument d'une procédure
+//*pointeur
+void jet_de_des(int *place, int const jet1, int const jet2){
+
+    *place += jet1+jet2;
 }
