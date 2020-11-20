bb07a7aac2bc47c115d3661ae1ac2a92d8cbb914
marine schwartz <m.schwartz@ludus-academie.com>
2020-11-20 10:29:14 +0100

    Update main.c

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//exemple: un pt dans un plan
+typedef struct tPoint{  // tPoint = nom du TYPE/structure
+
+    int x;
+    int y;
+
+}tPoint;
+
 int main()
 {
-    printf("Hello world!\n");
+
+    tPoint monPoint;
+    //tPoint monPoint={0,0};
+    //struct tPoint monPoint;
+
+    tPoint *pMonPoint=NULL;
+
+    pMonPoint=&monPoint;
+
+    monPoint.x=10;
+    monPoint.y=5;
+
+    //pointeur
+    pMonPoint->x=12;
+    pMonPoint->y=20;
+
+    //formalisme pointeur
+    (*pMonPoint).x=20;
+    (*pMonPoint).y=12;
+
     return 0;
 }
