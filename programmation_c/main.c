897187e87d0bd87b44a5efac89ca6d5ef185433c
marine schwartz <m.schwartz@ludus-academie.com>
2020-11-25 10:58:25 +0100

    SDL2
    init SDL
    window
    renderer
    SDL_Delay
    SDL_Quit

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
 
-int main()
+#define WINDOW_HEIGHT 1024
+#define WINDOW_WIDTH 1720
+
+SDL_Window *pWindow=NULL;
+SDL_Renderer *pRenderer=NULL;
+
+//prototype
+
+
+int main(int argc, char *argv[])
+//argv -> v = environnement
 {
-    printf("Hello world!\n");
+    if(SDL_Init(SDL_INIT_EVERYTHING)!=0){
+
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    } else {
+
+        pWindow=SDL_CreateWindow(   //http://wiki.libsdl.org/SDL_CreateWindow
+        "An SDL2 window",           // window title
+        0,                          // initial x position
+        0,                          // initial y position
+        WINDOW_WIDTH,               // width, in pixels
+        WINDOW_HEIGHT,              // height, in pixels
+        SDL_WINDOW_SHOWN            // flags
+        );
+
+        if(pWindow){
+
+            pRenderer=SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_PRESENTVSYNC);
+        }
+    }
+
+    SDL_Delay(1000);    //fenetre reste ouverte 1000ms (1s)
+
+    SDL_Quit();
+
+
     return 0;
 }
