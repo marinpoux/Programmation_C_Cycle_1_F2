2e75c7efa266ea79726bc46aae974e2aeae702dc
marine schwartz <m.schwartz@ludus-academie.com>
2020-12-03 09:58:53 +0100

    pointeurs sur fonctions
    pointeurs sur fonctions et pointeurs sur fonctions en argument

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,8 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+//<stdarg.h> --> pour creer methodes avec arguments variables/optionnels
+
+//les pointeurs sur fonctions
+
+//prototype
+    void affiche(int n_val);
+    int somme2int(int ent1, int ent2);
+
+void (*pAffiche)(int);  //<type_methode> (*<nom_pointeur>)(<type_argument_de_la_methode>)
+//void (*pAffiche2)(double);
+void (*pAffiche2)(void*);   //pointeur generique --> peut appeler n'importe quel type de donnee
+
+int (*pSomme2int)(int,int);
+
+int fctEntier(int (*pnf)(int), int n_val);
+
+int fctE(int n_a);
 
 int main()
 {
-    printf("Hello world!\n");
+    //affiche(12);
+    pAffiche=&affiche;  //assignation de la référence
+    //pAffiche2=&affiche2;
+
+    int n_val=0;
+    int p_int=&n_val;
+
+    pAffiche(12);
+
+    pSomme2int=&somme2int;
+    pSomme2int(4,2);
+
+    printf("\nSomme : %d", pSomme2int(4,2));
+
+    int res=fctEntier(&fctE,2);
+
     return 0;
+}
+
+void affiche(int n_val){
+
+    printf("%d", n_val);
+}
+
+int somme2int(int ent1, int ent2){
+
+    return (ent1 + ent2);
+}
+
+int fctEntier(int (*pnf)(int), int n_val){
+
+    return (*pnf)(n_val);
+}
+
+int fctE(int n_a){
+
+    return n_a;
 }
